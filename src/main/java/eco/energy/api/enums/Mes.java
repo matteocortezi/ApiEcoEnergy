4fed4cf6826df05b6b63eccaca5418d200e167ed
matteocortezi <developer.mateusv@gmail.com>
2024-11-14 17:10:20 -0300

    Criar enum mes

---
+++ src/main/java/eco/energy/api/enums/Mes.java
@@ -0,0 +1,16 @@
+package eco.energy.api.enums;
+
+public enum Mes {
+    JANEIRO,
+    FEVEREIRO,
+    MARCO,
+    ABRIL,
+    MAIO,
+    JUNHO,
+    JULHO,
+    AGOSTO,
+    SETEMBRO,
+    OUTUBRO,
+    NOVEMBRO,
+    DEZEMBRO;
+}
