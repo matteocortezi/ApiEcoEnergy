5ef19d3945d80d6a4ff01c503e7e1e9906c35b8d
matteocortezi <developer.mateusv@gmail.com>
2024-11-14 17:15:13 -0300

    Criar entidades de modelo

---
+++ src/main/java/eco/energy/api/model/Conta.java
@@ -0,0 +1,63 @@
+package eco.energy.api.model;
+import eco.energy.api.dto.contaDto.DadosAtualizacaoConta;
+import eco.energy.api.dto.contaDto.DadosCadastroConta;
+import eco.energy.api.enums.Mes;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Table(name = "Conta")
+@Entity(name = "Conta")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "idConta")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta", nullable = false)
+    private Long idConta;
+
+    @NotNull(message = "O valor total não pode ser nulo")
+    @DecimalMin(value = "0.01", message = "O valor total deve ser maior que 0")
+    @Column(name = "valor_conta", nullable = false)
+    private Double valorTotal;
+
+    @NotNull(message = "O consumo de kWh não pode ser nulo")
+    @DecimalMin(value = "0.01", message = "O consumo de kWh deve ser maior que 0")
+    @Column(name = "consumo_kwt_conta", nullable = false)
+    private Double consumoKwh;
+
+    @NotNull(message = "O mês não pode ser nulo")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "mes_conta", nullable = false)
+    private Mes mes;
+
+    @ManyToOne
+    @JoinColumn(name = "Usuario_id_usuario", nullable = false)
+    private Usuario usuario;
+
+
+
+    public Conta(DadosCadastroConta dados) {
+        this.valorTotal = dados.valorTotal();
+        this.consumoKwh = dados.consumoKwh();
+        this.mes = dados.mes();
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizacaoConta dados) {
+        if (dados.valorTotal() != null) {
+            this.valorTotal = dados.valorTotal();
+        }
+        if (dados.consumoKwh() != null) {
+            this.consumoKwh = dados.consumoKwh();
+        }
+        if (dados.mes() != null) {
+            this.mes = dados.mes();
+        }
+    }
+}
---
+++ src/main/java/eco/energy/api/model/Tarefa.java
@@ -0,0 +1,61 @@
+package eco.energy.api.model;
+import eco.energy.api.dto.tarefaDto.DadosAtualizacaoTarefa;
+import eco.energy.api.dto.tarefaDto.DadosCadastroTarefa;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Table(name = "Tarefa")
+@Entity(name = "Tarefa")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "idTarefa")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tarefa", nullable = false)
+    private Long idTarefa;
+
+    @NotBlank(message = "O nome da tarefa não pode ser vazio")
+    @Size(min = 3, max = 100, message = "O nome da tarefa deve ter entre 3 e 100 caracteres")
+    @Column(name = "nome_tarefa", nullable = false)
+    private String nome;
+
+    @NotBlank(message = "A descrição não pode ser vazia")
+    @Size(min = 5, max = 255, message = "A descrição deve ter entre 5 e 255 caracteres")
+    @Column(name = "descricao_tarefa", nullable = false)
+    private String descricao;
+
+    @NotNull(message = "O status não pode ser nulo")
+    @Column(name = "status_tarefa", nullable = true)
+    private Boolean status; // true para completada, false para não completada
+
+    @ManyToOne
+    @JoinColumn(name = "Usuario_id_usuario", nullable = false)
+    private Usuario usuario;
+
+    public Tarefa(DadosCadastroTarefa dados){
+        this.nome = dados.nome();
+        this.descricao = dados.descricao();
+        this.status = dados.status();
+    }
+    public void atualizarInformacoes(@Valid DadosAtualizacaoTarefa dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.descricao() != null) {
+            this.descricao = dados.descricao();
+        }
+        if (dados.status() != null) {
+            this.status = dados.status();
+
+        }}}
+
+
+
